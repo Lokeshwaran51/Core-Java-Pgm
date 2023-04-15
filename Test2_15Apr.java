53b659d801744714837d5c699a5d359d9a0a06a5
Lokeshwaran51 <130735629+Lokeshwaran51@users.noreply.github.com>
2023-04-15 12:16:35 +0530

    Add files via upload

---
+++ Test2_15Apr.java
@@ -0,0 +1,12 @@
+package com.EB_15;
+
+public class Test2_15Apr {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=15;
+		System.out.println("The Addition is:"+(a+b)+"\nThe Subtraction is:"+(a-b)+"\nThe Multiplication is:"+(a*b)+"\nThe Division is:"+(a/b));
+
+	}
+
+}
